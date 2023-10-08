0cc4bb5973a984e249bf74bf1d5758a39808796e
OmerEmreBozkurt <emre.bozkurt.29017@ozu.edu.tr>
2023-10-08 15:34:59 +0300

    A.

---
+++ src/Digit.java
@@ -0,0 +1,16 @@
+public class Digit {
+    private byte value;
+
+    public Digit(byte value) {
+        //There will be a constructor for Digit Class
+    }
+
+    public byte getValue() {
+        return value;
+    }
+
+    public void setValue(byte value) {
+        this.value = value;
+    }
+
+}
---
+++ src/LargeNumber.java
@@ -0,0 +1,34 @@
+public class LargeNumber {
+    private String longInt;
+    private Digit[] Digits; //All digits of long int will be stored in an Array
+
+    public LargeNumber (String longInt){
+        //there will be constructor for LargeNumber class
+    }
+
+    public void getDigits(String value){
+        //there will be a method to create an array from all digits of long int
+    }
+
+    public String add(LargeNumber longInt2){
+        // there will be a method to make addition
+        return null; //this will return a String like longInt
+    }
+    public String sub(LargeNumber longInt2){
+        // there will be a method to make subtraction
+        return null; //this will return a String like longInt
+    }
+    public String multiply(LargeNumber longInt2){
+        // there will be a method to make multiplication
+        return null; //this will return a String like longInt
+    }
+    public String divide(LargeNumber longInt2){
+        // there will be a method to make division
+        return null; //this will return a String like longInt
+    }
+
+    public LargeNumber digitsToLongInt(Digit digits){
+        //This will create a new String longInt from Digits array
+        return null;
+    }
+}
